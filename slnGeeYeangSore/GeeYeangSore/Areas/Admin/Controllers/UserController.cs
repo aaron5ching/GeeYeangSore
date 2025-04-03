425f952f0201c2d418d4589bb8637271042c05db
Rain Lin <xwm1593575@gmail.com>
2025-04-04 00:17:23 +0800

    建立使用者管理內的畫面資訊 新增ViewModels 及內部類別 CUserViewModels CUserViewModels.cshtml加入標籤架構 UserController加入邏輯

--- slnGeeYeangSore/GeeYeangSore/Areas/Admin/Controllers/UserController.cs
+++ slnGeeYeangSore/GeeYeangSore/Areas/Admin/Controllers/UserController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GeeYeangSore.Areas.Admin.ViewModels;
+using GeeYeangSore.Controllers;
+using GeeYeangSore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeeYeangSore.Areas.Admin.Controllers
 {
-    public class UserController : Controller
+    [Area("Admin")]
+    public class UserController : SuperController
     {
-        public IActionResult Index()
+
+        private readonly GeeYeangSoreContext _context;
+
+        public UserController(GeeYeangSoreContext context)
         {
-            return View();
+            _context = context;
         }
+
+        public IActionResult UserManagement()
+        {
+            if (!HasAnyRole("超級管理員", "系統管理員"))
+                return RedirectToAction("NoPermission", "Home", new { area = "Admin" });
+
+            var data = _context.HTenants
+                .Include(t => t.HLandlords)
+                .Select(t => new CUserViewModels
+                {
+                    TenantId = t.HTenantId,
+                    LandlordId = t.HLandlords.FirstOrDefault() != null ? t.HLandlords.First().HLandlordId.ToString() : "未啟用",
+                    Name = t.HUserName ?? "未填寫",
+                    RegisterDate = t.HCreatedAt ?? DateTime.MinValue,
+                    Status = t.HStatus ?? "未設定",
+                    IsTenant = t.HIsTenant ?? false,
+                    IsLandlord = t.HIsLandlord ?? false
+                }).ToList();
+
+            return View(data);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ slnGeeYeangSore/GeeYeangSore/Areas/Admin/ViewModels/CUserViewModels.cs
@@ -0,0 +1,16 @@
+﻿namespace GeeYeangSore.Areas.Admin.ViewModels
+{
+    public class CUserViewModels
+    {
+
+        public int TenantId { get; set; }
+        public string LandlordId { get; set; }
+        public string Name { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string Status { get; set; }
+        public bool IsTenant { get; set; }
+        public bool IsLandlord { get; set; }
+
+
+    }
+}
