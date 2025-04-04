ec8bb3a0889694f1bc0fe04e42b8fad6697716f1
Rain Lin <xwm1593575@gmail.com>
2025-04-04 18:17:09 +0800

    新增SearchUserViewModel.cs

---
+++ slnGeeYeangSore/GeeYeangSore/Areas/Admin/ViewModels/CSearchUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿// CUserSearchViewModel.cs
+namespace GeeYeangSore.Areas.Admin.ViewModels
+{
+    public class CUserSearchViewModel
+    {
+        public string? UserId { get; set; }
+        public string? Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? Status { get; set; }
+        public bool? IsTenant { get; set; }
+        public bool? IsLandlord { get; set; }
+    }
+}
