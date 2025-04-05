881de1efe1242ea9b24670e7a3f1f4134b9fa0e5
Jc <dbd1201085214@gmail.com>
2025-04-05 17:09:46 +0800

    0405-16:55

--- slnGeeYeangSore/GeeYeangSore/Areas/Admin/Controllers/AboutController.cs
+++ slnGeeYeangSore/GeeYeangSore/Areas/Admin/Controllers/AboutController.cs
@@ -54,19 +54,28 @@ namespace GeeYeangSore.Areas.Admin.Controllers
         {
 
             var about = _db.HAbouts.FirstOrDefault(item => item.HAboutId == HAboutId);
-            about.HTitle = HTitle;
-            about.HContent = HContent;
-            about.HUpdatedAt=DateTime.Now;
+
+            if (about != null)
+            {
+                about.HTitle = HTitle;
+                about.HContent = HContent;
+                about.HUpdatedAt = DateTime.Now;
+
+
+                _db.SaveChanges();
+                TempData["Success"] = "公告修改成功！";
+            }
 
             return RedirectToAction("About");
         }
         [HttpPost]
-        public IActionResult DeleteAbout(int HNewsId)
+        public IActionResult DeleteAbout(int HAboutId)
         {
 
-            var about = _db.HAbouts.FirstOrDefault(item => item.HAboutId == HNewsId);
+            var about = _db.HAbouts.FirstOrDefault(item => item.HAboutId == HAboutId);
             _db.Remove(about);
-
+            _db.SaveChanges();
+            TempData["Success"] = "公告刪除成功！";
             return RedirectToAction("About");
         }
 
