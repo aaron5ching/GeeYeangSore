97cd9c4352691045aa510e9c3887163b658c4b5d
Rain Lin <xwm1593575@gmail.com>
2025-04-24 11:43:57 +0800

    GeeYeangSoreContext.cs 與appsettings.json 修改伺服器路徑26.251.30.196

--- slnGeeYeangSore/GeeYeangSore/Models/GeeYeangSoreContext.cs
+++ slnGeeYeangSore/GeeYeangSore/Models/GeeYeangSoreContext.cs
@@ -91,7 +91,7 @@ public partial class GeeYeangSoreContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=26.232.199.225;Initial Catalog=GeeYeangSore;Persist Security Info=True;User ID=admin01;Password=admin01;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=26.251.30.196;Initial Catalog=GeeYeangSore;Persist Security Info=True;User ID=admin01;Password=admin01;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
