cba6cb79a74bd0c2aeac4b3c995e833180103d26
Jaycee <jcdbd214085@gmail.com>
2025-05-13 21:11:50 +0800

    新增 最新公告已可從後端api讀取

---
+++ slnGeeYeangSore/GeeYeangSore/APIControllers/Contact/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using GeeYeangSore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeeYeangSore.APIControllers.Contact;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContactController : ControllerBase
+{
+    private readonly GeeYeangSoreContext _db;
+
+    public ContactController(GeeYeangSoreContext db)
+    {
+        _db = db;
+    }
+
+    [HttpPost("contact")]
+    public IActionResult GetNews(object data)
+    {
+
+        Console.WriteLine(data);
+        return Ok(new { response = data});
+    }
+    
+    
+}
