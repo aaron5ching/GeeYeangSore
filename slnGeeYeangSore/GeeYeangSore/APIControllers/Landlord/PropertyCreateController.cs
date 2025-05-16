a2614535c2da7cd3ac63c554b2a693cba3d8355c
chillguy <fhge5698@gmail.com>
2025-05-16 10:23:47 +0800

    從訂基底

---
+++ slnGeeYeangSore/GeeYeangSore/APIControllers/Landlord/PropertyCreateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeeYeangSore.APIControllers.Landlord
+{
+    public class PropertyCreateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
